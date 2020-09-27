ebdb61ec8fd9f853a9ee7e938a5a821272a81874
Leon <qianthinking@gmail.com>
2020-09-27 09:32:34 +0800

    fix: ls illegal option in tmux

--- zsh/0000_before.zsh
+++ zsh/0000_before.zsh
@@ -1,5 +1,6 @@
 # Load any user customizations prior to load
 #
+# ensure this after coreutils PATH exported for tmux env
 if [ -d $HOME/.zsh.before/ ]; then
   if [ "$(ls -A $HOME/.zsh.before/)" ]; then
     for config_file ($HOME/.zsh.before/*.zsh) source $config_file
