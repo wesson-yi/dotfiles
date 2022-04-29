45af32400db77e7bbf43b385bd356ca18b7b4d36
Leon Li <qianthinking@gmail.com>
2022-04-29 21:44:40 +0800

    fix: noglob aliases

--- zsh/zsh-aliases.zsh
+++ zsh/zsh-aliases.zsh
@@ -10,10 +10,3 @@ alias -g S='| sort'
 alias -g G='| grep' # now you can do: ls foo G something
 
 alias -g be='bundle exec' #shorter, or see prezto/modules/ruby
-
-alias find='\find'
-alias rake='noglob rake'
-alias rsync='noglob rsync'
-alias scp='noglob scp'
-alias sftp='noglob sftp'
-
