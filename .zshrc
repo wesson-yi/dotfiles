fc43e5a5ecb7e212ef18b1ba7bf6d6b55a3c0c89
wesson-yi <wesson_yi@163.com>
2024-07-16 15:20:22 +0800

    zsh alias

--- .zshrc
+++ .zshrc
@@ -10,6 +10,18 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
+# 避免重复加载模块和函数
+zstyle ':prezto:load' pmodule 'prompt'
+zstyle ':prezto:load' pmodule 'git'
+
+# 异步加载模块
+zstyle ':prezto:module:async:load' tag-order 'prompt git'
+zstyle ':prezto:module:async:load' prompt 'async'
+zstyle ':prezto:module:async:load' git 'async'
+
+# 缓存设置
+zstyle ':prezto:module:prompt' prompt 'off'
+
 # Customize to your needs...
 for config_file ($HOME/.yadr/zsh/*.zsh) source $config_file
 
@@ -23,7 +35,6 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
 
 export PATH="/usr/local/opt/elasticsearch@6/bin:$PATH"
 export HOMEBREW_NO_AUTO_UPDATE=true
@@ -37,6 +48,19 @@ export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottl
 # objc[20451]: +[__NSPlaceholderDictionary initialize] may have been in progress in another thread when fork() was called.
 # objc[20451]: +[__NSPlac]
 # url: https://stackoverflow.com/questions/52671926/rails-may-have-been-in-progress-in-another-thread-when-fork-was-called
-export DISABLE_SPRING=true
+# export DISABLE_SPRING=true
 export RUBY_YJIT_ENABLE=1
 export PATH="/usr/local/opt/postgresql@13/bin:$PATH"
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+PATH=$(pyenv root)/shims:$PATH
+export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+
+# Java
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-1.8.jdk/Contents/Home
+PATH=$JAVA_HOME/bin:$PATH:.
+CLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:.
+export JAVA_HOME
+export PATH
+export CLASSPATH
--- zsh/aliases-ysn.zsh
+++ zsh/aliases-ysn.zsh
@@ -4,29 +4,45 @@ alias apps='cd ~/Web'
 alias app='cd ~/Web'
 alias .yadr='cd ~/.yadr'
 alias cloud='cd /Users/wesson/Library/Mobile\ Documents/com~apple~CloudDocs/Documents'
-alias rubyscript='cd ~/Web/workspace/ruby/script'
+alias rubyscript='cd ~/workspace/ruby/script'
 
 alias cls='clear;ls'
 alias la='ls -alGh'
 alias clt='clear;lt'
 alias ut='uptime'
 
+alias work='cd ~/workspace'
+alias study='cd ~/Study'
+
 alias alo='cd ~/Web/alo7'
 alias alo7='cd ~/Web/alo7'
 alias vkl='cd ~/Web/vcooline'
 alias vcl='cd ~/Web/vcooline'
 alias mkj='cd ~/Web/maikeji'
-alias work='cd ~/Web/workspace'
 alias feedmob='cd ~/Web/feedmob'
 alias feed='cd ~/Web/feedmob'
 alias hearing='cd ~/Web/hearing'
 alias sjx='cd ~/Web/sijixy'
+alias sijixy='cd ~/Web/sijixy'
+alias sijixing='cd ~/Web/sijixy'
+
 alias coop='cd ~/Web/sijixy/coop'
+alias coop_local='cd ~/Web/sijixy/coop_local'
+alias coop_fill_out='cd ~/Web/sijixy/coop_fill_out'
+
+alias medical='cd ~/Web/sijixy/coop_medical'
+alias coop_medical='cd ~/Web/sijixy/coop_medical'
+alias coop_medical_backend='cd ~/Web/sijixy/coop_medical_backend'
+alias backend='cd ~/Web/sijixy/coop_medical_backend'
+alias big_data='cd ~/Web/sijixy/big_data'
 alias program='cd ~/Program'
 
 alias stage='ssh root@$(basename `pwd`)'
-alias sy2='ssh -J wesson.yi@192.168.31.68 root@192.168.5.2'
-alias sy3='ssh -J wesson.yi@192.168.31.68 root@192.168.5.3'
+alias sy2='ssh -J wesson.yi@192.168.31.100 root@192.168.5.2'
+alias sy3='ssh -J wesson.yi@192.168.31.100 root@192.168.5.3'
+
+# alias sy2='ssh -J wesson.yi@192.168.64.25 root@192.168.5.2'
+# alias sy3='ssh -J wesson.yi@192.168.64.25 root@192.168.5.3'
 
 # Git
 alias gpp='git push origin $(git branch 2>/dev/null | grep "^\*" | sed -e "s/^\*\ //") -u'
@@ -45,6 +61,13 @@ alias cleans='git add .; git commit -m "CleanCode"'
 alias refactors='git add .; git commit -m "Refactor"'
 alias amends='git add .; git commit --amend --no-edit'
 
+alias sformats='git add .; SKIP=RuboCop git commit -m "FormatCode"'
+alias sfixs='git add .; SKIP=RuboCop git commit -m "FixBug"'
+alias sfix='SKIP=RuboCop git commit -m "FixBug"'
+# alias scleans='git add .; git commit -m "CleanCode"'
+# alias srefactors='git add .; git commit -m "Refactor"'
+# alias samends='git add .; git commit --amend --no-edit'
+
 # Git commit
 alias format='git commit -m "FormatCode"'
 alias fix='git commit -m "FixBug"'
@@ -57,6 +80,32 @@ function feature () { git checkout -b feature/$1 }
 
 # Rails
 alias brails='bundle exec rails'
+function drails() { docker exec -it coop-web-1 bash -c "cd /data/github/coop && bin/rails $*" }
+function dpumactl() { docker exec -it coop-web-1 bash -c "cd /data/github/coop && /usr/local/ruby-2.2.2/bin/pumactl $*" }
+function drake() { docker exec -it coop-web-1 bash -c "cd /data/github/coop && bin/rake $*" }
+
+docker-compose() {
+  if [ -f docker-compose.local.yml ]; then
+    command docker-compose -f docker-compose.local.yml "$@"
+  else
+    command docker-compose "$@"
+  fi
+}
+
+
+# rubocop() {
+#   if git ls-files -m | grep -q '\.rb$'; then
+#     git ls-files -m | grep '\.rb$' | xargs rubocop -a
+#   else
+#     command rubocop
+#   fi
+# }
+
+# tail
+alias tailf='tail -f'
+alias reloadZsh='source ~/.zshrc'
+alias editAlias='vim ~/.yadr/zsh/aliases-ysn.zsh'
+alias reloadAlias='source ~/.zshrc'
 
 # Tmux
 alias tmx='tmux attach -t'
